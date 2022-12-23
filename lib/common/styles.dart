37dcc1b51b56c417756ca256ff9c7d24a11c49e8
triagung128 <triagung128@gmail.com>
2022-12-23 21:06:11 +0700

    setup styles theme & create login page

---
+++ lib/common/styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color blueColor = Color(0xFF1A72DD);
+const Color navyColor = Color(0xFF2A3256);
+const Color whiteColor = Color(0xFFFFFFFF);
+
+TextStyle blueTextStyle = GoogleFonts.rubik(color: blueColor);
+TextStyle navyTextStyle = GoogleFonts.rubik(color: navyColor);
+TextStyle whiteTextStyle = GoogleFonts.rubik(color: whiteColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
---
+++ lib/presentation/pages/login_page.dart
@@ -0,0 +1,95 @@
+import 'package:fic_mini_project/common/styles.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Log in'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.only(
+          top: 40,
+          left: 24,
+          right: 24,
+        ),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Palm Cafe POS',
+                style: navyTextStyle.copyWith(
+                  fontSize: 24,
+                  fontWeight: medium,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                'Silahkan login terlebih dahulu sebagai vendor atau member untuk melanjutkan.',
+                style: navyTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 50),
+              Center(
+                child: Image.asset(
+                  'assets/logo.png',
+                  width: 162,
+                  height: 187,
+                ),
+              ),
+              const SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(MediaQuery.of(context).size.width, 57),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: Text(
+                  'Log in as Vendor',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 12),
+              Center(
+                child: Text(
+                  'atau',
+                  style: navyTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  fixedSize: Size(MediaQuery.of(context).size.width, 57),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                child: Text(
+                  'Log in as Member',
+                  style: whiteTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
