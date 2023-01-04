43cdf14e3c9fe1553df186cfefe381363239f595
triagung128 <triagung128@gmail.com>
2023-01-04 07:50:40 +0700

    added grey color

--- lib/common/styles.dart
+++ lib/common/styles.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 const Color blueColor = Color(0xFF1A72DD);
 const Color navyColor = Color(0xFF2A3256);
 const Color whiteColor = Color(0xFFFFFFFF);
+const Color greyColor = Color(0xFFF7F8FA);
 
 final textTheme = TextTheme(
   headline1: GoogleFonts.rubik(
