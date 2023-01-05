c90cb31a7836f5eba65d2bc73d6a7ba46bc0d454
triagung128 <triagung128@gmail.com>
2023-01-05 19:25:49 +0700

    added form validator update profile

--- lib/presentation/pages/profile/update_profile_page.dart
+++ lib/presentation/pages/profile/update_profile_page.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
+import 'package:email_validator/email_validator.dart';
 import 'package:fic_mini_project/common/styles.dart';
 import 'package:fic_mini_project/domain/entity/user.dart';
 import 'package:fic_mini_project/presentation/blocs/profile/profile_bloc.dart';
-import 'package:fic_mini_project/presentation/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
@@ -35,7 +35,7 @@ class _UpdateProfilePageState extends State<UpdateProfilePage> {
               ..hideCurrentSnackBar()
               ..showSnackBar(
                 SnackBar(
-                  backgroundColor: Colors.red,
+                  backgroundColor: Colors.red[400],
                   content: Text(state.message),
                 ),
               );
@@ -46,16 +46,17 @@ class _UpdateProfilePageState extends State<UpdateProfilePage> {
               ..hideCurrentSnackBar()
               ..showSnackBar(
                 SnackBar(
+                  backgroundColor: Colors.green[400],
                   content: Text(state.message),
                 ),
               );
           }
         },
         child: BlocBuilder<ProfileBloc, ProfileState>(
-          buildWhen: (previous, current) =>
+          buildWhen: (_, current) =>
               current is! SelectImageSuccess &&
               current is! UpdateProfileLoading,
-          builder: (context, state) {
+          builder: (_, state) {
             if (state is FetchProfileLoading) {
               return const Center(
                 child: CircularProgressIndicator(),
@@ -88,6 +89,8 @@ class _ContentUpdateProfile extends StatelessWidget {
   final _emailController = TextEditingController();
   final _phoneNumberController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     _nameController.text = user.name ?? '';
@@ -97,122 +100,191 @@ class _ContentUpdateProfile extends StatelessWidget {
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.all(24),
-        child: Column(
-          children: [
-            InkWell(
-              borderRadius: BorderRadius.circular(16),
-              onTap: () async {
-                final imagePicker = ImagePicker();
-                await imagePicker
-                    .pickImage(source: ImageSource.gallery)
-                    .then((image) {
-                  if (image != null) {
-                    context.read<ProfileBloc>().add(SelectImage(image));
-                  }
-                });
-              },
-              child: SizedBox(
-                height: 100,
-                width: 100,
-                child: Stack(
-                  children: [
-                    BlocBuilder<ProfileBloc, ProfileState>(
-                      buildWhen: (previous, current) =>
-                          current is! UpdateProfileLoading,
-                      builder: (_, state) {
-                        if (state is SelectImageSuccess) {
-                          return CircleAvatar(
-                            radius: 50,
-                            backgroundColor: Colors.grey[300],
-                            backgroundImage: FileImage(
-                              File(state.image.path),
+        child: Form(
+          key: _formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(16),
+                  onTap: () async {
+                    final imagePicker = ImagePicker();
+                    await imagePicker
+                        .pickImage(source: ImageSource.gallery)
+                        .then((image) {
+                      if (image != null) {
+                        context.read<ProfileBloc>().add(SelectImage(image));
+                      }
+                    });
+                  },
+                  child: SizedBox(
+                    height: 100,
+                    width: 100,
+                    child: Stack(
+                      children: [
+                        BlocBuilder<ProfileBloc, ProfileState>(
+                          buildWhen: (_, current) =>
+                              current is! UpdateProfileLoading,
+                          builder: (_, state) {
+                            if (state is SelectImageSuccess) {
+                              return CircleAvatar(
+                                radius: 50,
+                                backgroundColor: Colors.grey[300],
+                                backgroundImage: FileImage(
+                                  File(state.image.path),
+                                ),
+                              );
+                            } else {
+                              return user.photoUrl != null
+                                  ? CircleAvatar(
+                                      radius: 50,
+                                      backgroundColor: Colors.grey[300],
+                                      backgroundImage:
+                                          NetworkImage(user.photoUrl!),
+                                    )
+                                  : CircleAvatar(
+                                      radius: 50,
+                                      backgroundColor: Colors.grey[300],
+                                      child: const Icon(Icons.person, size: 48),
+                                    );
+                            }
+                          },
+                        ),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: Container(
+                            padding: const EdgeInsets.all(4),
+                            decoration: BoxDecoration(
+                              color: blueColor,
+                              borderRadius: BorderRadius.circular(8),
                             ),
-                          );
-                        } else {
-                          return user.photoUrl != null
-                              ? CircleAvatar(
-                                  radius: 50,
-                                  backgroundColor: Colors.grey[300],
-                                  backgroundImage: NetworkImage(user.photoUrl!),
-                                )
-                              : CircleAvatar(
-                                  radius: 50,
-                                  backgroundColor: Colors.grey[300],
-                                  child: const Icon(Icons.person, size: 48),
-                                );
-                        }
-                      },
-                    ),
-                    Align(
-                      alignment: Alignment.bottomRight,
-                      child: Container(
-                        padding: const EdgeInsets.all(4),
-                        decoration: BoxDecoration(
-                          color: blueColor,
-                          borderRadius: BorderRadius.circular(8),
+                            child: const Icon(
+                              Icons.edit,
+                              color: whiteColor,
+                              size: 18,
+                            ),
+                          ),
                         ),
-                        child: const Icon(
-                          Icons.edit,
-                          color: whiteColor,
-                          size: 18,
-                        ),
-                      ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 30),
-            CustomTextField(
-              controller: _nameController,
-              labelText: 'Nama',
-              hintText: 'Masukkan Nama',
-              keyboardType: TextInputType.name,
-              textCapitalization: TextCapitalization.words,
-            ),
-            const SizedBox(height: 20),
-            CustomTextField(
-              controller: _emailController,
-              labelText: 'Email',
-              hintText: 'Masukkan Email',
-              keyboardType: TextInputType.emailAddress,
-            ),
-            const SizedBox(height: 20),
-            CustomTextField(
-              controller: _phoneNumberController,
-              labelText: 'No. Handphone',
-              hintText: 'Masukkan No. Handphone',
-              keyboardType: TextInputType.phone,
-            ),
-            const SizedBox(height: 50),
-            BlocBuilder<ProfileBloc, ProfileState>(
-              builder: (context, state) {
-                return ElevatedButton.icon(
-                  onPressed: state is UpdateProfileLoading
-                      ? null
-                      : () {
-                          final dataInput = User(
-                            id: user.id,
-                            name: _nameController.text,
-                            email: _emailController.text,
-                            phoneNumber: _phoneNumberController.text,
-                            photoUrl: user.photoUrl,
-                          );
-                          context
-                              .read<ProfileBloc>()
-                              .add(UpdateProfile(dataInput));
-                        },
-                  icon: state is UpdateProfileLoading
-                      ? const CircularProgressIndicator()
-                      : const Icon(Icons.save),
-                  label: const Text('Simpan'),
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: Size(MediaQuery.of(context).size.width, 57),
-                  ),
-                );
-              },
-            ),
-          ],
+              const SizedBox(height: 30),
+              Text(
+                'Nama',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1!
+                    .copyWith(color: navyColor, fontWeight: FontWeight.w700),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  hintText: 'Masukkan Nama',
+                ),
+                keyboardType: TextInputType.name,
+                textCapitalization: TextCapitalization.words,
+                validator: (value) {
+                  if (value.toString().isEmpty) {
+                    return 'Nama tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Email',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1!
+                    .copyWith(color: navyColor, fontWeight: FontWeight.w700),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  hintText: 'Masukkan Email',
+                ),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  if (value.toString().isEmpty) {
+                    return 'Email tidak boleh kosong';
+                  }
+                  if (value != null && !EmailValidator.validate(value)) {
+                    return 'Email tidak valid';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'No. Handphone',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1!
+                    .copyWith(color: navyColor, fontWeight: FontWeight.w700),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                controller: _phoneNumberController,
+                decoration: const InputDecoration(
+                  hintText: 'Masukkan No. Handphone',
+                ),
+                keyboardType: TextInputType.phone,
+                validator: (value) {
+                  if (value.toString().isEmpty) {
+                    return 'No. Handphone tidak boleh kosong';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 50),
+              BlocBuilder<ProfileBloc, ProfileState>(
+                builder: (context, state) {
+                  return ElevatedButton.icon(
+                    onPressed: state is UpdateProfileLoading
+                        ? null
+                        : () {
+                            if (_formKey.currentState!.validate()) {
+                              final dataInput = User(
+                                id: user.id,
+                                name: _nameController.text,
+                                email: _emailController.text,
+                                phoneNumber: _phoneNumberController.text,
+                                photoUrl: user.photoUrl,
+                              );
+                              context
+                                  .read<ProfileBloc>()
+                                  .add(UpdateProfile(dataInput));
+                            } else {
+                              ScaffoldMessenger.of(context)
+                                ..hideCurrentSnackBar()
+                                ..showSnackBar(
+                                  SnackBar(
+                                    content: const Text(
+                                      'Harap lengkapi isian form !',
+                                    ),
+                                    backgroundColor: Colors.red[400],
+                                  ),
+                                );
+                            }
+                          },
+                    icon: state is UpdateProfileLoading
+                        ? const CircularProgressIndicator()
+                        : const Icon(Icons.save),
+                    label: const Text('Simpan'),
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: Size(MediaQuery.of(context).size.width, 57),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
